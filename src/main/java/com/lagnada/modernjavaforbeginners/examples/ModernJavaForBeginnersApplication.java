3d81726d481cf96b78002a80fbfa206e38daeed3
Tony Lagnada <deloyz@gmail.com>
2024-02-02 13:16:11 -0800

    pom updates and maven wrapper.

---
+++ src/main/java/com/lagnada/modernjavaforbeginners/examples/ModernJavaForBeginnersApplication.java
@@ -0,0 +1,13 @@
+package com.lagnada.modernjavaforbeginners.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaForBeginnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaForBeginnersApplication.class, args);
+	}
+
+}
